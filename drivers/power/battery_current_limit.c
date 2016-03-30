36e2c5e61dc8f2b64867527ee65e6d2a92c7576d
Tommy-Geenexus <roboter972@gmail.com>
2016-03-30 16:20:14 +0200

    power: bcl: update bcl_cpu_online_mask upon hotplug operations * The following issue is occuring during boot:
    	- bcl driver		-> is initialized
    	- bcl_handle_hotplug()	-> called
    	- bcl_handle_hotplug()	-> bcl_cpu_online_mask is updated
    	- msm_performance	-> big cluster is shut down from userspace context
    	- bcl_handle_hotplug()	-> called
    	- bcl_handle_hotplug()	-> cpumask is not updated and contains previously offlined big CPUs
    	- bcl_handle_hotplug()	-> does not skip CPUs offlined via msm_performance
    	- bcl_handle_hotplug()	-> tries to online big CPUs
    	- msm_performance	-> disallows onlining of big CPUs
    	- bcl_handle_hotplug()	-> cpu_up() fails with -EPERM error
    
    * The solution: Update the bcl_cpu_online_mask upon hotplug operations:
    
    	- bcl driver		-> is initialized
    	- bcl_handle_hotplug()	-> called
    	- bcl_handle_hotplug()	-> bcl_cpu_online_mask is updated
    	- msm_performance	-> big cluster is shut down from userspace context
    	- bcl_handle_hotplug()	-> called
    	- bcl_handle_hotplug()	-> bcl_cpu_online_mask is updated
    	- bcl_handle_hotplug()	-> cpumask correctly contains only currently online CPUs
    	- bcl_handle_hotplug()	-> skips onlining big CPUs as they are not set in bcl_cpu_online_mask

--- drivers/power/battery_current_limit.c
+++ drivers/power/battery_current_limit.c
@@ -220,14 +220,19 @@ static void bcl_update_online_mask(void)
 				cpumask_weight(bcl_cpu_online_mask));
 }
 
+#define bcl_prev_num_online_cpus()	cpumask_weight(bcl_cpu_online_mask)
+
+static int bcl_cpumask_outdated(void)
+{
+	return bcl_prev_num_online_cpus() != num_online_cpus();
+}
+
 #ifdef CONFIG_SMP
 static void __ref bcl_handle_hotplug(struct work_struct *work)
 {
 	int ret = 0, _cpu = 0;
 
 	mutex_lock(&bcl_hotplug_mutex);
-	if (cpumask_empty(bcl_cpu_online_mask))
-		bcl_update_online_mask();
 
 #ifdef CONFIG_MSM_BCL_SOMC_CTL
 	if ((bcl_soc_state == BCL_LOW_THRESHOLD
@@ -243,6 +248,9 @@ static void __ref bcl_handle_hotplug(struct work_struct *work)
 #endif /* CONFIG_MSM_BCL_SOMC_CTL */
 	else
 		bcl_hotplug_request = 0;
+
+	if (bcl_cpumask_outdated() || cpumask_empty(bcl_cpu_online_mask))
+		bcl_update_online_mask();
 
 	for_each_possible_cpu(_cpu) {
 		if ((!(bcl_hotplug_mask & BIT(_cpu))
@@ -288,6 +296,8 @@ static int __ref bcl_cpu_ctrl_callback(struct notifier_block *nfb,
 
 	switch (action & ~CPU_TASKS_FROZEN) {
 	case CPU_UP_PREPARE:
+		if (cpumask_empty(bcl_cpu_online_mask))
+			bcl_update_online_mask();
 		if (!cpumask_test_and_set_cpu(cpu, bcl_cpu_online_mask))
 			pr_debug("BCL online Mask: %u\n",
 				cpumask_weight(bcl_cpu_online_mask));
@@ -957,12 +967,12 @@ mode_store(struct device *dev, struct device_attribute *attr,
 		return -EPERM;
 
 	if (!strcmp(buf, "enable")) {
-		bcl_update_online_mask();
 		bcl_mode_set(BCL_DEVICE_ENABLED);
 		pr_info("bcl enabled\n");
 	} else if (!strcmp(buf, "disable")) {
 		bcl_mode_set(BCL_DEVICE_DISABLED);
-		cpumask_clear(bcl_cpu_online_mask);
+		if (!cpumask_empty(bcl_cpu_online_mask))
+			cpumask_clear(bcl_cpu_online_mask);
 		pr_info("bcl disabled\n");
 	} else {
 		return -EINVAL;
