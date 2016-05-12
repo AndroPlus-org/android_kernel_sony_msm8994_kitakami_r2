66765ac41f3b252da586e34bf7bb818e2a6f480f
alucard24 <dmbaoh2@gmail.com>
2016-05-12 22:31:52 +0200

    Imported Alucard CPU Governor!

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -438,6 +438,9 @@ extern struct cpufreq_governor cpufreq_gov_elementalx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
 #endif
 
 /*********************************************************************
