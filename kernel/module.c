47a0aaff2cc0cf5d35d4eab3cca92bc3e6f1b7d0
AndroPlus <hosigumayuugi@gmail.com>
2016-09-30 21:47:20 +0900

    module: Force load core_ctl

--- kernel/module.c
+++ kernel/module.c
@@ -1180,6 +1180,9 @@ static int check_version(Elf_Shdr *sechdrs,
 	if(!strncmp("ecryptfs", mod->name, 8))
 		return 1;
 
+	if(!strncmp("core_ctl", mod->name, 8))
+		return 1;
+
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
